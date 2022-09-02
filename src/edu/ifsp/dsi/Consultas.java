ef8be906c81902be37d27dfe556c765f21c22236
Professor LDA2 <professor@lda2.arq.ifsp.edu.br>
2022-09-02 09:27:13 -0300

    consultas - ineficiente

--- src/edu/ifsp/dsi/Consultas.java
+++ src/edu/ifsp/dsi/Consultas.java
@@ -4,26 +4,39 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Consultas {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		
 		try (Connection conn = ConnectionProvider.getConnection()) {
-					
-			try (Statement stmt = conn.createStatement();) {
+		
+			System.out.println("Digite 0 (zero) para sair.");
+			System.out.println("Preço máximo: ");
+			double precoMaximo = sc.nextInt();
 
-				ResultSet rs = stmt.executeQuery(
-						"SELECT id, descricao, preco "
-						+ "FROM produtos WHERE preco < 3000;");
-				
-				while (rs.next()) {
-					long id = rs.getLong("id"); 
-					String descricao = rs.getString("descricao");
-					double preco = rs.getDouble("preco");
+			while (precoMaximo > 0) {
+				try (Statement stmt = conn.createStatement();) {
+	
+					ResultSet rs = stmt.executeQuery(
+							"SELECT id, descricao, preco "
+							+ "FROM produtos WHERE preco < " + precoMaximo + ";");
 					
-					System.out.printf("%d\t%s\t%.2f\n", 
-							id, descricao, preco);
+					while (rs.next()) {
+						long id = rs.getLong("id"); 
+						String descricao = rs.getString("descricao");
+						double preco = rs.getDouble("preco");
+						
+						System.out.printf("%d\t%s\t%.2f\n", 
+								id, descricao, preco);
+					}
 				}
+				
+				System.out.println("Preço máximo: ");
+				precoMaximo = sc.nextInt();
 			}
 			
 		} catch (SQLException e) {
